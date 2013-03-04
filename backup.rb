cd695b19816c41764060329bab14203c4fc860ec
Bilal Budhani <bilalbudhani@gmail.com>
2013-03-04 13:31:23 +0530

    change of variables

--- backup.rb
+++ backup.rb
@@ -4,8 +4,8 @@ require './backup_class.rb'
 
 # Application settings
 settings = Hash.new
-settings[:username] = 'bilalbudhani@gmail.com'		 	# Gmail username 
-settings[:password] = 'qobgnbeljdbvnkuu'				# Gmail password
+settings[:username] = 'xxx@gmail.com'		 	# Gmail username 
+settings[:password] = 'your password'				# Gmail password
 settings[:folder] 	= 'emails_backup'					# Folder to store emails
 settings[:after]	= Date.today - 30					# Before this date
 
