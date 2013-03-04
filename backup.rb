9d27f26daf9edc456eddcfc18d74c2f2e83c4916
Bilal Budhani <bilalbudhani@gmail.com>
2013-03-04 13:24:20 +0530

    Thread support added

--- backup.rb
+++ backup.rb
@@ -1,10 +1,11 @@
+require 'ap'
 require './backup_class.rb'
 
 
 # Application settings
 settings = Hash.new
-settings[:username] = 'xxx@gmail.com'		 			# Gmail username 
-settings[:password] = 'gmail password'					# Gmail password
+settings[:username] = 'bilalbudhani@gmail.com'		 	# Gmail username 
+settings[:password] = 'qobgnbeljdbvnkuu'				# Gmail password
 settings[:folder] 	= 'emails_backup'					# Folder to store emails
 settings[:after]	= Date.today - 30					# Before this date
 
--- backup_class.rb
+++ backup_class.rb
@@ -1,10 +1,60 @@
 require 'gmail'
 
+module Net
+  class IMAP
+    class ResponseParser 
+      def msg_att
+        match(T_LPAR)
+        attr = {}
+        while true
+          token = lookahead
+          case token.symbol
+          when T_RPAR
+            shift_token
+            break
+          when T_SPACE
+            shift_token
+            token = lookahead
+          end
+          case token.value
+          when /\A(?:ENVELOPE)\z/ni
+            name, val = envelope_data
+          when /\A(?:FLAGS)\z/ni
+            name, val = flags_data
+          when /\A(?:INTERNALDATE)\z/ni
+            name, val = internaldate_data
+          when /\A(?:RFC822(?:\.HEADER|\.TEXT)?)\z/ni
+            name, val = rfc822_text
+          when /\A(?:RFC822\.SIZE)\z/ni
+            name, val = rfc822_size
+          when /\A(?:BODY(?:STRUCTURE)?)\z/ni
+            name, val = body_data
+          when /\A(?:UID)\z/ni
+            name, val = uid_data
+
+          when /\A(?:X-GM-MSGID)\z/ni  # Added X-GM-MSGID extension
+            name, val = uid_data
+          when /\A(?:X-GM-THRID)\z/ni  # Added X-GM-THRID extension
+            name, val = uid_data
+
+          else
+            parse_error("unknown attribute `%s'", token.value)
+          end
+          attr[name] = val
+        end
+        return attr
+      end
+
+    end
+  end
+end
+
 class BackupGmail
 
 	def initialize(settings)
 		@settings = settings
-		@gmail = Gmail.new(@settings[:username], @settings[:password])	
+		@gmail = Gmail.new(@settings[:username], @settings[:password])		
+		#@gmail.peek = true
 		dirCheck
 
 	end
@@ -17,16 +67,29 @@ class BackupGmail
 	end
 
 	def backup
-		@gmail.inbox.emails(:after => @settings[:after]).each do |email|
-			save_email(email)
-		end
+		emails = @gmail.inbox.emails(:after => @settings[:after]).each do |email|
+				uid = email.uid
+				data = @gmail.conn.uid_fetch(uid, "X-GM-THRID")
+				save_email(email, data[0].attr["X-GM-THRID"])
+		end 
+
 	end
 
 
-	def save_email(email)
-		f = File.new("#{@settings[:folder]}/#{email.uid}.txt", "w")
-		f.write(email.body)
-		f.close
+	def save_email(email, thread_id)
+		filename = "#{@settings[:folder]}/#{thread_id}.txt"
+		if !File.exists?(filename)
+				f  = File.new(filename, "w")
+				f.write(email.body)
+				f.close()
+			else 	
+				f = File.open(filename, "a")  
+				f.write("\n-------------------- NEXT EMAIL -----------------------\n")
+				f.write(email.body)
+				f.close()
+		end	
+
 	end
+
 
 end
